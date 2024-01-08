1b78e7c081457e459a2392dd4d80fa1dd5b518ad
Ranjit Raut <67329253+theranjitraut@users.noreply.github.com>
2024-01-08 13:54:38 +0545

    Update 19binarysearching.c

--- 19binarysearching.c
+++ 19binarysearching.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int binarySearch(int a[ ], int low, int high, int key)
+#define size 10
+int binarysearching(int a[ ], int low, int high, int key)
 {
     int mid;
     while (high >= low)
@@ -7,7 +8,7 @@ int binarySearch(int a[ ], int low, int high, int key)
         mid = (low+high)/2;
         if(key == a[mid])
         {
-            printf(“Search is successful\n”);
+            printf("Search is successful\n");
             return mid+1;
         }
         else if(key < a[mid])
@@ -19,6 +20,20 @@ int binarySearch(int a[ ], int low, int high, int key)
             low = mid+1;
         }
     }
-    printf(“Item not found\n”);
+    printf("Search is unsuccessful\n");
     return -1;
+}
+int main()
+{
+    int a[size], n, key;
+    printf("Enter no. of entires: ");
+    scanf("%d", &n);
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter %d number: ", i+1);
+        scanf("%d", &a[i]);
+    }
+    printf("Enter key to find: ");
+    scanf("%d", &key);
+    printf("The location is: %d\n", binarysearching(a, 0, n, key));
 }
